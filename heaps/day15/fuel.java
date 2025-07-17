fced61270572e982fa5aaae4c0d6bc9e587d90b0
abrar <mohammedabrar934@gmail.com>
2025-07-17 10:48:04 +0530

    d15 first ques

---
+++ heaps/day15/fuel.java
@@ -0,0 +1,31 @@
+package heaps;
+
+import java.util.PriorityQueue;
+
+public class fuel {
+    class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int fuel = startFuel;
+        int result=0;
+        int refuels=0;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+        int i =0;
+        while(fuel<target){
+            while(i<stations.length && stations[i][0]<=fuel){
+                maxHeap.add(stations[i][1]);
+                i++;
+            }
+            if(maxHeap.isEmpty()){
+                return -1;
+            }
+
+            fuel+= maxHeap.poll();
+            refuels++;
+
+        }
+        return refuels;
+        
+    }
+}
+    
+}
